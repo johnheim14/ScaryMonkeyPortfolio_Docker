de58ba8bb6fa0df1cebea1294892fda40ab746e5
John Heim <pe2312ff@go.minnstate.edu>
2025-08-13 16:21:45 -0500

    Change back to Tailwind, integrate automated CSS building into runner actions.

---
+++ web/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Scan for classes in your source files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
